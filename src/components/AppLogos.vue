6eb4c01841ee6974f40480f519c4fdced4f9c677
wildgrid <zm3345@126.com>
2024-07-01 16:40:49 +0800

    feat: space-xxx not work

--- src/components/AppLogos.vue
+++ src/components/AppLogos.vue
@@ -1,10 +1,10 @@
 <template>
-  <view inline-flex cursor-default text-2xl font-300>
+  <view inline-flex cursor-default text-2xl font-300 class="space-x-24rpx">
     <view
       flex
       flex-col
       items-center
-      hover-class="drop-shadow-md drop-shadow-color-green5 space-x-12rpx"
+      hover-class="drop-shadow-md drop-shadow-color-green5"
     >
       <image inline-block h-18 w-18 src="/static/logo.svg" />
       <text mt--2 text-green5>
@@ -13,10 +13,10 @@
     </view>
     <view
       text="3xl gray4"
-      m="x-4 y-auto"
+      m="y-auto"
       i-carbon-add transform transition-all-500 hover:rotate-135
     />
-    <view flex flex-col hover-class="drop-shadow-md drop-shadow-color-purple5">
+    <view flex flex-col hover-class="drop-shadow-md drop-shadow-color-purple5" class="space-x-12rpx">
       <image inline-block h-18 w-18 src="/static/vite.png" />
       <text mt--2 text-purple5>
         Vite
